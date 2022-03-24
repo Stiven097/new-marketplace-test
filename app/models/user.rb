e7f27d0f45e756811ce91594904196773f75c629
Stiven097 <s@delonix.io>
2022-03-24 17:46:23 -0400

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :full_name, presence: true, length: {maximun: 50} 
+  validates :full_name, presence: true, length: {maximum: 50} 
 end
