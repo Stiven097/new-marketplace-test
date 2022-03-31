3bcd2929108ec7432d8b064ee8a6e794e51fc475
Stiven097 <s@delonix.io>
2022-03-31 16:32:44 -0400

    registrations controller, added route, set to update without password

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  devise_for :users, path: '', path_names: {sign_up: 'register',
-                                            sign_in: 'login',
-                                            edit: 'profile',
-                                            sign_out: 'logout'}
+  devise_for :users,
+              path: '',
+              path_names: {sign_up: 'register',sign_in: 'login',edit: 'profile',sign_out: 'logout'},
+              controllers: {registrations:'registrations'}
 end
