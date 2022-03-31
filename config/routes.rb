4b3b8c1aab1a46a3c17eb9388a66d0e207c0bc9c
Stiven097 <s@delonix.io>
2022-03-31 15:53:47 -0400

    changed routes of authentication pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  devise_for :users
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {sign_up: 'register',
+                                            sign_in: 'login',
+                                            edit: 'profile',
+                                            sign_out: 'logout'}
 end
