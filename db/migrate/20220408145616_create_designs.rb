c87f40ad61813182a8a0d564b58ff033a3e1849e
Stiven097 <s@delonix.io>
2022-04-08 11:33:48 -0400

    created category, design and pricing models

---
+++ db/migrate/20220408145616_create_designs.rb
@@ -0,0 +1,14 @@
+class CreateDesigns < ActiveRecord::Migration[6.0]
+  def change
+    create_table :designs do |t|
+      t.string :title
+      t.string :video
+      t.boolean :active, default: false
+      t.boolean :has_single_pricing, default: false
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
