a588767886a1bda6c2eb6d8f3e82b79467d3f898
Stiven097 <s@delonix.io>
2022-04-13 16:40:43 -0400

    update method functional. all the steps set in case of an error

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -2,6 +2,7 @@ class DesignsController < ApplicationController
   before_action :authenticate_user!, except: [:show]
   before_action :set_design, except: [:new, :create]
   before_action :is_authorised, only: [:edit, :update]
+  before_action :set_step, only: [:update, :edit]
 
   def new
     @design = current_user.designs.build
@@ -21,16 +22,73 @@ class DesignsController < ApplicationController
 
   def edit
     @categories = Category.all
-    @step = params[:step].to_i
   end
 
   def update
+
+    if @step == 2
+      design_params[:pricings_attributes].each do |index, pricing|
+        if @design.has_single_pricing && pricing[:pricing_type] != Pricing.pricing_types.key(0)
+          next;
+        else
+          if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
+            return redirect_to request.referrer, flash: {error: "Invalid pricing"}
+          end
+        end
+      end
+    end
+
+    if @step == 3 && design_params[:description].blank?
+      return redirect_to request.referrer, flash: {error: "Description cannot be blank."}
+    end
+
+    if @step == 4 && @design.photos.blank?
+      return redirect_to request.referrer, flash: {error: "You don't have any photos."}
+    end
+
+    if @step == 5
+      @design.pricings.each do |pricing|
+        if @design.has_single_pricing && !pricing.basic?
+          next;
+        else
+          if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
+            return redirect_to edit_design_path(@design, step: 2), flash: {error: "Invalid pricing."}
+          end
+        end
+      end
+
+      if @design.description.blank?
+        return redirect_to edit_design_path(@design, step: 3), flash: {error: "Description cannot be blank."}
+      elsif @design.photos.blank?
+        return redirect_to edit_design_path(@design, step: 4), flash: {error: "You don't have any photos."}
+      end
+    end
+
+    if @design.update(design_params)
+      flash[:notice] = "Saved!"
+    else
+      return redirect_to request.referrer, flash: {error: @design.errors.full_messages}
+    end
+
+    if @step < 5
+      redirect_to edit_design_path(@design, step: @step + 1)
+    else
+      redirect_to dashboard_path
+    end
+
   end
 
   def show
   end
 
   private
+
+  def set_step
+    @step = params[:step].to_i > 0 ? params[:step].to_i : 1
+    if @step > 5
+      @step = 5
+    end
+  end
 
   def set_design
     @design = Design.find(params[:id])
@@ -41,7 +99,7 @@ class DesignsController < ApplicationController
   end
 
   def design_params
-    params.require(:design).permit(:title, :video, :active, :category_id, :has_single_pricing,
+    params.require(:design).permit(:title, :video, :description, :active, :category_id, :has_single_pricing,
                                     pricings_attributes: [:id, :title, :description, :delivery_time, :price, :pricing_type])
   end
 end
