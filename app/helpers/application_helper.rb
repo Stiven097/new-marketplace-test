e39ebb9ed78cfb5755e7353470c1f34b49c88690
Stiven097 <s@delonix.io>
2022-04-18 01:55:03 -0400

    created helper for design covers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       ActionController::Base.helpers.asset_path('icon_default_avatar.png')
     end
   end
+
+  def design_cover(design)
+    if design.photos.attached?
+      url_for(design.photos[0])
+    else
+      ActionController::Base.helpers.asset_path('icon_default_image.png')
+    end
+  end
 end
