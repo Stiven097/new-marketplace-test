39ee60168927266bb9a6e285ba37c88011255d9b
Stiven097 <s@delonix.io>
2022-04-23 20:11:58 -0400

    added relations between orders and requests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :design, required: false
+  belongs_to :request, required: false
 
   belongs_to :buyer, class_name: "User"
   belongs_to :seller, class_name: "User"
