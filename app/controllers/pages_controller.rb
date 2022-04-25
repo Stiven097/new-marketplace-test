7ee908bbace3f1108ba584938dac1c69878ed6d2
Stiven097 <s@delonix.io>
2022-04-25 15:22:49 -0400

    created flexible condition to filter the search of a design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,24 @@ class PagesController < ApplicationController
   def search
     @categories = Category.all
     @category = Category.find(params[:category]) if params[:category].present?
+
+    #@designs = Design.where("active = ? AND designs.title ILIKE ? AND category_id = ?", true, "%#{params[:q]}%", params[:category])
+
+    @q = params[:q]
+
+    query_condition = []
+    query_condition[0] = "designs.active = true"
+
+    if !@q.blank?
+      query_condition[0] += " AND designs.title ILIKE ?"
+      query_condition.push "%#{@q}%"
+    end
+
+    if !params[:category].blank?
+      query_condition[0] += " AND category_id = ?"
+      query_condition.push params[:category]
+    end
+
+    @designs = Design.where(query_condition)
   end
 end
