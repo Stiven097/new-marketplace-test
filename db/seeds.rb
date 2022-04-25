e4eacf81b334f3b38590541fdffb0e7308a4bb2e
Stiven097 <s@delonix.io>
2022-04-25 13:15:30 -0400

    dummy data for designs assigned to random users

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,15 +34,53 @@
 
 # Create dummy Requests
 
+# 10.times do
+  #  random_user = User.all.sample(1)[0]
+  #  category = Category.all.sample(1)[0]
+  #  request = Request.create(
+  #      title: Faker::Job.title,
+  #      description: Faker::Quote.matz,
+  #      budget: Faker::Number.between(5, 50),
+  #      delivery: Faker::Number.between(1, 10),
+  #      user_id: random_user.id,
+  #      category_id: category.id
+  #  )
+# end
+
 10.times do
     random_user = User.all.sample(1)[0]
     category = Category.all.sample(1)[0]
-    request = Request.create(
-        title: Faker::Job.title,
+    design = Design.create(
+        title: Faker::Job.unique.title,
         description: Faker::Quote.matz,
-        budget: Faker::Number.between(5, 50),
-        delivery: Faker::Number.between(1, 10),
+        active: Faker::Boolean.boolean,
         user_id: random_user.id,
         category_id: category.id
+    )
+    number = Faker::Number.between(1, 3)
+    design.photos.attach(
+        io: File.open("app/assets/images/design_cover_#{number}.jpg"),
+        filename: "category_#{number}.jpeg"
+    )
+    design.pricings.create(
+        pricing_type: 0,
+        title: Faker::Job.title,
+        description: Faker::Quote.matz,
+        price: Faker::Number.between(1, 9),
+        delivery_time: Faker::Number.between(20, 30),
+    )
+    design.pricings.create(
+        pricing_type: 1,
+        title: Faker::Job.title,
+        description: Faker::Quote.matz,
+        price: Faker::Number.between(10, 19),
+        delivery_time: Faker::Number.between(10, 19),
+    )
+    design.pricings.create(
+        pricing_type: 2,
+        title: Faker::Job.title,
+        description: Faker::Quote.matz,
+        price: Faker::Number.between(20, 35),
+        delivery_time: Faker::Number.between(1, 10),
     )
 end
