e9bf1bc5900de4f3388efd721cb0913be37a1056
Stiven097 <s@delonix.io>
2022-04-27 13:04:32 -0400

    config update payment method for the card information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,32 @@ class UsersController < ApplicationController
     redirect_to dashboard_path
   end
 
+  def update_payment
+    if !current_user.stripe_id
+      customer = Stripe::Customer.create(
+        email: current_user.email,
+        source: params[:stripeToken],
+        expand: ['sources']
+      )
+    else
+      customer = Stripe::Customer.update(
+        current_user.stripe_id,
+        source: params[:stripeToken]
+      )
+    end
+
+    if current_user.update(stripe_id: customer.id, stripe_last_4: customer.sources.data.first["last4"])
+      flash[:notice] = "Card information added successfully"
+    else
+      flash[:alert] = "Invalid card information"
+    end
+
+    redirect_to request.referrer
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to request.referrer
+  end
+
   private
   def current_user_params
     params.require(:user).permit(:from, :about, :status, :language, :avatar)
