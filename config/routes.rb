823651f498959bb24504e8aaf9036a8c5d4fba9b
Stiven097 <s@delonix.io>
2022-04-29 01:53:14 -0400

    added route for the checkout page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/search', to: 'pages#search'
   get '/settings/payment', to: 'users#payment', as: 'settings_payment'
   get '/settings/payout', to: 'users#payout', as: 'settings_payout'
+  get '/designs/:id/checkout/:pricing_type', to: 'designs#checkout', as: 'checkout'
 
   post '/users/edit', to: 'users#update'
   post '/offers', to: 'offers#create'
