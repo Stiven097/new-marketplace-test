52c8757dd5fc6a7946c2541adfc60849e16e11ea
Stiven097 <s@delonix.io>
2022-04-29 12:16:48 -0400

    variables to earnings method to handle the amounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,37 @@ class UsersController < ApplicationController
     redirect_to request.referrer
   end
 
+  def earnings
+    @net_income = (Transaction.where("seller_id = ?", current_user.id).sum(:amount) / 1.1).round(2)
+
+    @withdrawn = Transaction.where("buyer_id = ? AND status = ? AND transaction_type = ?", 
+                    current_user.id, 
+                    Transaction.statuses[:approved],
+                    Transaction.transaction_types[:withdraw]
+                  ).sum(:amount)
+
+    @pending = Transaction.where("buyer_id = ? AND status = ? AND transaction_type = ?", 
+                    current_user.id, 
+                    Transaction.statuses[:pending],
+                    Transaction.transaction_types[:withdraw]
+                  ).sum(:amount)
+
+    @purchased = Transaction.where("buyer_id = ? AND source = ? AND transaction_type = ?", 
+                    current_user.id, 
+                    Transaction.sources[:system],
+                    Transaction.transaction_types[:trans]
+                  ).sum(:amount)
+
+    @withdrawable = current_user.wallet
+
+    @transactions = Transaction.where("seller_id = ? OR (buyer_id = ? AND source = ?)", 
+                      current_user.id,
+                      current_user.id,
+                      Transaction.sources[:system]
+                ).page(params[:page])
+
+  end
+  
   private
   def current_user_params
     params.require(:user).permit(:from, :about, :status, :language, :avatar)
