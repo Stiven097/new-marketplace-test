a605820154579cd0a97b19ae4ba1cb81062377a8
Stiven097 <s@delonix.io>
2022-05-04 01:10:50 -0400

    deleted unnecessary code

--- app/admin/categories_admin.rb
+++ app/admin/categories_admin.rb
@@ -44,20 +44,4 @@ Trestle.resource(:categories) do
     text_field :name
   end
 
-  #   row do
-  #     col(xs: 6) { datetime_field :updated_at }
-  #     col(xs: 6) { datetime_field :created_at }
-  #   end
-  # end
-
-  # By default, all parameters passed to the update and create actions will be
-  # permitted. If you do not have full trust in your users, you should explicitly
-  # define the list of permitted parameters.
-  #
-  # For further information, see the Rails documentation on Strong Parameters:
-  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
-  #
-  # params do |params|
-  #   params.require(:category).permit(:name, ...)
-  # end
 end
