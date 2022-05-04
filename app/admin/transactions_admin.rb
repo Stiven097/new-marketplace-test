f4efe6a1825634cc91d15e1f22df846d044d4858
Stiven097 <s@delonix.io>
2022-05-04 01:42:09 -0400

    added transactions admin page

---
+++ app/admin/transactions_admin.rb
@@ -0,0 +1,24 @@
+Trestle.resource(:transactions) do
+  remove_action :destroy
+
+  menu do
+    item :transactions, icon: "fa fa-money", label: "Widthdraw"
+  end
+
+  collection do
+    Transaction.withdraw
+  end
+
+  table do
+    column :buyer
+    column :amount
+    column :status
+    column :transaction_type
+    column :created_at, align: :center
+
+    actions do |toolbar, instance, admin|
+      
+    end
+  end
+
+end
