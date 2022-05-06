1496026f55b6c6fb066ece208926653e1dc395ff
Stiven097 <s@delonix.io>
2022-05-06 01:48:00 -0400

    added condition for subscription fees

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -99,6 +99,15 @@ class DesignsController < ApplicationController
   end
 
   def checkout
+
+    subscription = Subscription.find_by_user_id(current_user.id)
+    if subscription.present? && subscription.success?
+      plan = Stripe::Plan.retrieve(subscription.plan_id)
+      @rate = plan.metadata.commission.to_f/100
+    else
+      @rate = 10.0/100
+    end
+
     if current_user.stripe_id
       @stripe_customer = Stripe::Customer.retrieve({
         id: current_user.stripe_id, 
